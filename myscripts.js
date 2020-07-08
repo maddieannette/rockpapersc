b8c81ec1adf23e0f6f0e748a9e41af32f4238693
maddieannette <madeline.a.flores@gmail.com>
2020-07-07 22:36:52 -0500

    Added js to rock button

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,19 @@
-let myArray = ["Rock", "Paper", "Scissors"];
-let randomItem = myArray[Math.floor(Math.random()*myArray.length)];
-document.body.innerHTML += randomItem;
+let gameChoice = ['buttonrock', 'buttonpaper', 'buttonscissors'];
+let round = 0;
+
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * gameChoice.length);
+    return gameChoice[randomNumber];
+}
+
+function playRound(playerSelection) {
+    let computerSelection=computerPlay(); 
+    if (playerSelection === 'buttonrock' && computerSelection === 'buttonscissors' ||
+        playerSelection === 'buttonpaper' && computerSelection === 'buttonrock' ||
+        playerSelection === 'buttonscissors' && computerSelection === 'buttonpaper')
+        document.getElementById("result").innerHTML="YOU WIN";
+        return 'YOU WIN!';
+}
+const playerSelection = 'buttonrock'
+const computerSelection = computerPlay()
+// console.log(playRound(playerSelection, computerSelection))
